48143b19060e18780388d5710836675d0fffe972
Alexandr Stafeev <Staff208397@yandex.ru>
2023-04-26 12:52:54 +0300

    26/04 12:53

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,28 +1,20 @@
 
 from rest_framework import mixins
-from rest_framework.response import Response
-from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework import filters
-from rest_framework import status, viewsets
-
-from django.shortcuts import get_object_or_404
-
+from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from api.permissions import IsAdminOrReadOnly
-
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
 from django.core.exceptions import PermissionDenied
 from reviews.models import Genre, Title, Category
 from api.serializers import (
     GenreSerializer, TitleSerializer,
-    CategorySerializer, TitleCreateSerializer,
-    CommentSerializer, ReviewSerializer)
-from reviews.models import Genre, Title, Category, Comment, Review
+    CategorySerializer, TitleCreateSerializer)
+from reviews.models import Genre, Title, Category
 from api.serializers import (GenreSerializer,
                              TitleSerializer,
                              CategorySerializer)
-from django.shortcuts import render
 
 
 class TitleViewSet(viewsets.ModelViewSet):
@@ -56,32 +48,4 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
-
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    serializer_class = CommentSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, )
-
-    def get_title(self):
-        self.title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-        return self.title
-
-    def get_queryset(self):
-        title = self.get_title()
-        new_queryset = title.comments.all()
-        return new_queryset
-
-    def perform_create(self, serializer):
-        title = self.get_title()
-        serializer.save(author=self.request.user, title=title)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save(author=self.request.user, title=title)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ReviewViewSet(viewsets.ModelViewSet):
-    serializer_class = ReviewSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, )
 
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,72 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-class User(AbstractUser):
-
-
-    ADMIN = 'admin'
-    MODERATOR = 'moderator'
-    USER = 'user'
-
-    ROLE_CHOICES =  [
-        (ADMIN, ADMIN),
-        (MODERATOR, MODERATOR),
-        (USER, USER),
-    ]
-
-    username = models.CharField(
-        'Логин',
-        unique=True,
-        blank=False,
-        max_length=150,
-    )
-    email = models.EmailField(
-        'e-mail адрес',
-        unique=True,
-        blank=False,
-        max_length=50,
-    )
-    first_name = models.CharField(
-        'Имя',
-        blank=False,
-        max_length=150,
-    )
-    last_name = models.CharField(
-        'Фамилия',
-        blank=True,
-        max_length=150,
-    )
-    bio = models.TextField(
-        'О пользователе',
-        blank=True,
-    )
-    role = models.CharField(
-        choices=ROLE_CHOICES,
-        default='user',
-        max_length=10,
-    )
-    
-    @property
-    def is_admin(self):
-        return self.role == self.ADMIN
-    
-    @property
-    def is_moderator(self):
-        return self.role == self.MODERATOR
-    
-    class Meta:
-        ordering = ('id',)
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-
-
-    def __str__(self) -> str:
-        return self.username
 
 
 class Category(models.Model):
+
     name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
 
@@ -80,6 +17,7 @@ class Category(models.Model):
 
 
 class Genre(models.Model):
+
     name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
 
@@ -93,7 +31,6 @@ class Genre(models.Model):
 
 
 class Title(models.Model):
-
 
     name = models.CharField(max_length=250, help_text='Название произведения')
     categories = models.ForeignKey(
@@ -109,11 +46,11 @@ class Title(models.Model):
     year = models.PositiveIntegerField(help_text='Год выхода произведения')
     rating = models.PositiveIntegerField(null=True, default=None)
 
+
     def __str__(self):
         return self.name
 
     class Meta:
-
         ordering = ('-year',)
         constraints = [
             models.UniqueConstraint(
@@ -121,58 +58,3 @@ class Title(models.Model):
                 name='unique_name_categories'
             )
         ]
-
-
-
-class Review(models.Model):
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='reviews')
-    title = models.ForeignKey(Title,
-                              on_delete=models.CASCADE,
-                              related_name='reviews')
-    score = models.PositiveSmallIntegerField(
-        'Оценка',
-        validators=[
-            MaxValueValidator(10),
-            MinValueValidator(1)
-        ]
-    )
-    text = models.TextField('Текст отзыва')
-    pub_date = models.DateTimeField('Дата добавления',
-                                    auto_now_add=True,
-                                    db_index=True)
-    
-
-    def __str__(self):
-        return self.text[:15]
-
-    class Meta:
-        ordering = ('-pub_date',)
-        constraints = [
-            models.UniqueConstraint(
-                fields=['author', 'title'],
-                name='unique_review'
-            )
-        ]
-
-
-class Comment(models.Model):
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='comments')
-    review = models.ForeignKey(Review,
-                               on_delete=models.CASCADE,
-                               related_name='comments',
-                               null=True,
-                               blank=True)
-    text = models.TextField('Текст комментария')
-    pub_date = models.DateTimeField('Дата добавления',
-                                    auto_now_add=True,
-                                    db_index=True)
-
-    def __str__(self):
-        return self.text[:15]
-
-    class Meta:
-        ordering = ('-pub_date',)
